140745a539531b76cf1b48ad98df48c3c1ee1184
dorutrip <dorutrip@gmail.com>
2022-06-20 16:52:49 +0300

    facts WIP

--- src/components/blocks/Facts/FactsView.jsx
+++ src/components/blocks/Facts/FactsView.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
 import { Image } from 'semantic-ui-react';
 import { UniversalLink } from '@plone/volto/components';
-import lightBulb from '@package/static/logo.svg';
+import suitCase from '@package/static/suitcase.svg';
+import area from '@package/static/area.svg';
+import people from '@package/static/people.svg';
+import gradHat from '@package/static/gradhat.svg';
+import home from '@package/static/home.svg';
 import cx from 'classnames';
 
 import './facts.less';
@@ -28,20 +32,20 @@ const FactsView = (props) => {
             View all
           </UniversalLink>
         </div>
-        <Card icon={lightBulb} size="big" title="130" subtitle="Businesses" />
+        <Card icon={suitCase} size="big" title="130" subtitle="Businesses" />
         <div className="subfacts-grid">
-          <Card icon={lightBulb} title="51.000" subtitle="Students per day" />
+          <Card icon={gradHat} title="51.000" subtitle="Students per day" />
           <Card
-            icon={lightBulb}
+            icon={home}
             title="3.000"
             subtitle="Student accomodations"
           />
         </div>
       </div>
       <div className="facts-row">
-        <Card icon={lightBulb} title="27000" subtitle="Staff members per day" />
+        <Card icon={people} title="27000" subtitle="Staff members per day" />
         <Card
-          icon={lightBulb}
+          icon={area}
           title="322"
           subtitle="Surface area in hectares"
         />

8a74f01527932697f258e6b2d7d7306141a096bf
Tiberiu Ichim <tiberiu.ichim@gmail.com>
2022-06-22 15:45:05 +0300

    Add formatted date export

--- src/components/index.js
+++ src/components/index.js
@@ -2,3 +2,4 @@ export StickyHeader from './theme/Header/StickyHeader';
 export HeroSection from './theme/Header/HeroSection';
 export PreviewImage from './theme/PreviewImage/PreviewImage';
 export ResponsiveContainer from './theme/ResponsiveContainer/ResponsiveContainer';
+export FormattedDate from './FormattedDate/FormattedDate';

4f768d254a03fa35f833e4a0f35a604135e069ab
Tiberiu Ichim <tiberiu.ichim@gmail.com>
2022-06-27 13:02:31 +0300

    Only show one carousel variation

--- src/components/blocks/ImageCards/index.js
+++ src/components/blocks/ImageCards/index.js
@@ -2,7 +2,7 @@ import ImageCarousel from './Carousel';
 
 export default function install(config) {
   config.blocks.blocksConfig.imagecards.blockRenderers = {
-    ...config.blocks.blocksConfig.imagecards.blockRenderers,
+    // ...config.blocks.blocksConfig.imagecards.blockRenderers,
     imageCarousel: {
       title: 'Image carousel',
       schema: null,

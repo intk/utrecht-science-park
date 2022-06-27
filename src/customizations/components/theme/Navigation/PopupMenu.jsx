97e7c1df0ca4c959e78857b4584544213d010041
Tiberiu Ichim <tiberiu.ichim@gmail.com>
2022-06-27 14:00:01 +0300

    New navigation implementation

--- src/customizations/components/theme/Navigation/PopupMenu.jsx
+++ src/customizations/components/theme/Navigation/PopupMenu.jsx
@@ -2,12 +2,13 @@ import React from 'react';
 import { Portal } from 'react-portal';
 import { CSSTransition } from 'react-transition-group';
 import PropTypes from 'prop-types';
+import cx from 'classnames';
 import { doesNodeContainClick } from 'semantic-ui-react/dist/commonjs/lib';
 
 const DEFAULT_TIMEOUT = 500;
 
 const PopupMenu = (props) => {
-  const { children, open, onClose } = props;
+  const { children, open, onClose, className } = props;
 
   const asideElement = React.createRef();
 
@@ -24,32 +25,30 @@ const PopupMenu = (props) => {
   });
 
   return (
-    <>
-      <CSSTransition
-        in={open}
-        timeout={DEFAULT_TIMEOUT}
-        classNames="popup-menu"
-        unmountOnExit
-      >
-        <Portal>
-          <div
-            role="presentation"
-            onClick={(e) => {
-              e.stopPropagation();
-            }}
-            onKeyDown={(e) => {
-              e.stopPropagation();
-            }}
-            ref={asideElement}
-            key="popupmenu"
-            className="popup-menu"
-            style={{ overflowY: 'auto' }}
-          >
-            {children}
-          </div>
-        </Portal>
-      </CSSTransition>
-    </>
+    <CSSTransition
+      in={open}
+      timeout={DEFAULT_TIMEOUT}
+      classNames="popup-menu"
+      unmountOnExit
+    >
+      <Portal>
+        <div
+          role="presentation"
+          onClick={(e) => {
+            e.stopPropagation();
+          }}
+          onKeyDown={(e) => {
+            e.stopPropagation();
+          }}
+          ref={asideElement}
+          key="popupmenu"
+          className={cx('popup-menu', className)}
+          style={{ overflowY: 'auto' }}
+        >
+          {children}
+        </div>
+      </Portal>
+    </CSSTransition>
   );
 };
 
--- src/customizations/components/theme/Navigation/TopLevelItems.jsx
+++ src/customizations/components/theme/Navigation/TopLevelItems.jsx
@@ -1,46 +1,107 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-import NavItems from './NavItems';
+import { Icon } from '@plone/volto/components';
 import config from '@plone/volto/registry';
+
+import _ from 'lodash';
+
+import NavItems from './NavItems';
 import PopupMenu from './PopupMenu';
+
+import downKeySVG from '@plone/volto/icons/down-key.svg';
+
+const HOME = ['', '/', '/en', '/nl'];
+
+const isMouseOver = (node, e) => {
+  // const contains = node && node.contains(e.target);
+  // console.log('mouse out', contains, e);
+
+  const { clientX, clientY } = e;
+  if (_.some([clientX, clientY], _.isNil)) return false;
+
+  // false if the node is not visible
+  const clientRects = node.getClientRects();
+  // Heads Up!
+  // getClientRects returns a DOMRectList, not an array nor a plain object
+  // We explicitly avoid _.isEmpty and check .length to cover all possible shapes
+  if (
+    !node.offsetWidth ||
+    !node.offsetHeight ||
+    !clientRects ||
+    !clientRects.length
+  )
+    return false;
+
+  // false if the node doesn't have a valid bounding rect
+  const { top, bottom, left, right } = _.first(clientRects);
+  if (_.some([top, bottom, left, right], _.isNil)) return false;
+
+  // we add a small decimal to the upper bound just to make it inclusive
+  // don't add an whole pixel (1) as the event/node values may be decimal sensitive
+  return (
+    _.inRange(clientY, top, bottom + 0.001) &&
+    _.inRange(clientX, left, right + 0.001)
+  );
+};
 
 export default function TopLevelItems(props) {
   const { items, lang } = props;
   const { settings } = config;
-  const [showMenu, setShowMenu] = React.useState(false);
+
   const handleClick = React.useCallback((e) => {
-    setShowMenu(false);
+    setTopItem(null);
     // e.preventDefault();
     // e.stopPropagation();
   }, []);
 
+  const handleMouseOut = React.useCallback((e) => {
+    const over = isMouseOver(nodeRef.current, e);
+
+    if (!over) {
+      console.log('out', over);
+      setTopItem(null);
+    }
+  }, []);
+
+  const [topItem, setTopItem] = React.useState();
+  const nodeRef = React.useRef();
+
   return (
     <>
-      {items.map((item) => (
-        <NavLink
-          to={item.url === '' ? '/' : item.url}
-          key={item.url}
-          activeClassName="active"
-          className="item"
-          exact={
-            settings.isMultilingual ? item.url === `/${lang}` : item.url === ''
-          }
-          onMouseOver={() => setShowMenu(true)}
-          onFocus={() => setShowMenu(true)}
-          onKeyDown={handleClick}
-          onClick={handleClick}
-        >
-          {item.title}
-        </NavLink>
-      ))}
-      <PopupMenu open={showMenu} onClose={() => setShowMenu(false)}>
-        <div tabIndex="-1" role="button" className="hover-menu">
-          <div className="ui container">
-            <NavItems
-              items={props.items?.slice(1)}
-              lang={props.lang}
-              onClose={() => setShowMenu(false)}
-            />
+      {items
+        .filter((item) => HOME.indexOf(item.url) === -1)
+        .map((item) => (
+          <NavLink
+            to={item.url === '' ? '/' : item.url}
+            key={item.url}
+            activeClassName="active"
+            className="item"
+            exact={
+              settings.isMultilingual
+                ? item.url === `/${lang}`
+                : item.url === ''
+            }
+            onMouseOver={() => setTopItem(item)}
+            onFocus={() => setTopItem(item)}
+            onKeyDown={handleClick}
+            onClick={handleClick}
+          >
+            {item.title}
+            <Icon name={downKeySVG} size="24px" />
+          </NavLink>
+        ))}
+      <PopupMenu open={!!topItem} onClose={() => setTopItem(null)}>
+        <div className="hover-menu" ref={nodeRef} onMouseOut={handleMouseOut}>
+          <div tabIndex="-1" role="button" className="hover-menu-inner">
+            <div className="ui container">
+              {!!topItem && (
+                <NavItems
+                  items={topItem.items || []}
+                  lang={props.lang}
+                  onClose={() => setTopItem(null)}
+                />
+              )}
+            </div>
           </div>
         </div>
       </PopupMenu>

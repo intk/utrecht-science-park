d9bf23f82ff3ca6eb51c1177315b834c1732c53b
Tiberiu Ichim <tiberiu.ichim@gmail.com>
2022-07-13 19:25:24 +0300

    Use listing body for ListingView

--- src/components/theme/View/ListingView.jsx
+++ src/components/theme/View/ListingView.jsx
@@ -1,17 +1,20 @@
 import React from 'react';
 import { Container } from 'semantic-ui-react'; // Segment,
 import { BodyClass } from '@plone/volto/helpers';
-import ListingTemplate from '@package/components/blocks/Listing/ListingTemplate';
+// import ListingTemplate from '@package/components/blocks/Listing/ListingTemplate';
+import ListingBody from '@plone/volto/components/manage/Blocks/Listing/ListingBody';
 
-const ListingView = ({ content }) => (
-  <Container id="page-folder">
-    <BodyClass className="multiple-content-view" />
-    <section id="content-core">
-      <div className="listings">
-        <ListingTemplate items={content.items} />
-      </div>
-    </section>
-  </Container>
-);
+const ListingView = ({ content, location: { pathname } }) => {
+  return (
+    <Container id="page-folder">
+      <BodyClass className="multiple-content-view" />
+      <section id="content-core">
+        <div className="listings">
+          <ListingBody properties={content} data={{}} path={pathname} />
+        </div>
+      </section>
+    </Container>
+  );
+};
 
 export default ListingView;

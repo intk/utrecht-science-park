0086bb9dda68ff90f26b32779b3ea6ae244ff0eb
dorutrip <dorutrip@gmail.com>
2023-05-17 16:35:02 +0300

    fix multilingual

--- src/config.js
+++ src/config.js
@@ -25,7 +25,7 @@ export default function applyConfig(config) {
 
   const DEFAULT_LANG = 'en';
 
-  config.settings.isMultilingual = false;
+  config.settings.isMultilingual = true;
   config.settings.supportedLanguages = ['en', 'nl'];
   config.settings.defaultLanguage = DEFAULT_LANG;
 
